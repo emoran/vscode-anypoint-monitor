26d5ce248adf0fe3ecfdcd66b20a577f25d56310
Yucel Moran <yucel.moran@gmail.com>
2024-12-29 08:41:43 -0500

    Add webtab

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,17 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('a', () => {
+	const disposable = vscode.commands.registerCommand('anypoint-monitor.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
-		// Display a message box to the unypoint-monitor.helloWorldser
+		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from anypoint-monitor!!');
+		var panel = vscode.window.createWebviewPanel(
+			'toolbox',
+			'Home',
+			vscode.ViewColumn.One,
+			{}
+		);
+		//panel.webview.html = getWebviewContent();
 	});
 
 	context.subscriptions.push(disposable);
